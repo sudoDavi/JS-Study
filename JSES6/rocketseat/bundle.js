fb2c04b2cc8614b6690ea37187224732855520b1
Davi Wendt <daviwendt@gmail.com>
2020-02-24 20:47:55 -0300

    Array operations

--- JSES6/rocketseat/bundle.js
+++ JSES6/rocketseat/bundle.js
@@ -1,11 +1,19 @@
 "use strict";
 
-function teste(x) {
-  var y = 2;
-
-  if (x > 5) {
-    console.log(x, y);
-  }
-}
-
-console.log(teste(10));
+var arr = [1, 3, 4, 5, 8, 10];
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter);
+var find = arr.find(function (item) {
+  return item === 2;
+});
+console.log(find);
--- JSES6/rocketseat/main.js
+++ JSES6/rocketseat/main.js
@@ -1,10 +1,25 @@
-function teste(x) {
-    let y = 2;
+const arr = [1,3,4,5,8,10];
 
-    if(x > 5) {
-        console.log(x, y);
-    }
-}
+const newArr = arr.map(function(item, index) {
+    return item+index;
+});
 
-console.log(teste(10));
+console.log(newArr);
 
+const sum = arr.reduce(function(total, next) {
+return total+next;
+});
+
+console.log(sum);
+
+const filter = arr.filter(function(item) {
+return item % 2 === 0;
+});
+
+console.log(filter);
+
+const find = arr.find(function(item) {
+    return item === 2;
+});
+
+console.log(find);
