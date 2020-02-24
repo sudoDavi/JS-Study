6afc26a6eb5361e21ea96543aac73462962171b2
Davi Wendt <daviwendt@gmail.com>
2020-02-23 21:10:49 -0300

    Modelo de produto

--- Nodejs/rocketseat/index.js
+++ Nodejs/rocketseat/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require("require-dir");
 
+//Start app
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-});
+//Start DB
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi", 
+    {useNewUrlParser: true, useUnifiedTopology: true}
+);
+requireDir("./src/models");
+
+
+
+//Rotas
+app.use('/', require('./src/routes'))
 
 app.listen(3001);
