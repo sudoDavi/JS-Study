901b5d9842c84525870006a7477daaf07864ed20
Davi Wendt <daviwendt@gmail.com>
2020-02-24 21:17:21 -0300

    Template literals

--- JSES6/rocketseat/main.js
+++ JSES6/rocketseat/main.js
@@ -1,33 +1,5 @@
-//REST
+const nome = "moido";
+const idade = 0;
 
-// const usuario = {
-//     nome: "Davi",
-//     idade: 23,
-//     empresa: "Nenhuma",
-//     instituicao: "IFSUL"
-// };
-
-// const { nome, ...resto } = usuario;
-
-// console.log(nome);
-// console.log(resto);
-
-
-const soma = (a, b, ...params) => params;
-
-console.log(soma(1, 3 ,4));
-
-
-//SPREAD
-
-const usuario = {
-    nome: "Davi",
-    idade: 23,
-    empresa: "Nenhuma",
-    instituicao: "IFSUL"
-};
-
-const usuario2 = { ...usuario, nome: "Alan"};
-
-console.log(usuario2);
-
+// console.log("Meu nome é "+ nome + " e tenho "+ idade+" anos");
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
