f5135a08acc25970427350bb2d5eabdf01d080ac
Davi Wendt <daviwendt@gmail.com>
2020-02-25 14:33:17 -0300

    rebase jsEs6-challenges

--- JSES6/rocketseat/bundle.js
+++ JSES6/rocketseat/bundle.js
@@ -108,48 +108,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 "use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _functions__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./functions */ \"./functions.js\");\n\nconsole.log(Object(_functions__WEBPACK_IMPORTED_MODULE_0__[\"soma\"])(1, 2));\n\n//# sourceURL=webpack:///./main.js?");
 
-<<<<<<< HEAD
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var usuarios = [{
-  nome: 'Diego',
-  idade: 23,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Gabriel',
-  idade: 15,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Lucas',
-  idade: 30,
-  empresa: 'Facebook'
-}];
-var idade = usuarios.map(function (item) {
-  return item.idade;
-});
-console.log(idade);
-var funcRocket = usuarios.filter(function (item) {
-  return item.empresa === 'Rocketseat' && item.idade >= 18;
-});
-console.log(funcRocket);
-var funcGoogle = usuarios.find(function (item) {
-  return item.empresa === 'Google';
-});
-console.log(funcGoogle);
-var idadeX2 = usuarios.map(function (usuario) {
-  return _objectSpread({}, usuario, {
-    idade: usuario.idade * 2
-  });
-}).filter(function (usuario) {
-  return usuario.idade <= 50;
-});
-console.log(idadeX2);
-=======
 /***/ })
 
 /******/ });
->>>>>>> configurando webpack
--- JSES6/rocketseat/main.js
+++ JSES6/rocketseat/main.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-const usuarios = [  
-    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },  
-    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },  
-    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
-];
-
-const idade = usuarios.map(item => item.idade);
-console.log(idade);
-
-const funcRocket = usuarios.filter(item => item.empresa === 'Rocketseat' && item.idade >= 18);
-console.log(funcRocket);
-
-const funcGoogle = usuarios.find(item => item.empresa === 'Google');
-console.log(funcGoogle);
-
-const idadeX2 = usuarios.map(usuario => (
-        {...usuario, idade: usuario.idade * 2})
-    ).filter(usuario => usuario.idade <=50);
-console.log(idadeX2);
-=======
 import { soma } from './functions';
 
 console.log(soma(1, 2));
->>>>>>> configurando webpack
