b3318298a692ccbdd8e2eb723f5e72d90e738072
Davi Wendt <daviwendt@gmail.com>
2020-02-24 21:00:27 -0300

    Desestruturação

--- JSES6/rocketseat/bundle.js
+++ JSES6/rocketseat/bundle.js
@@ -1,19 +1,18 @@
 "use strict";
 
-var arr = [1, 3, 4, 5, 8, 10];
-var newArr = arr.map(function (item, index) {
-  return item + index;
-});
-console.log(newArr);
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sum);
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter);
-var find = arr.find(function (item) {
-  return item === 2;
-});
-console.log(find);
+var user = {
+  nome: "Davi",
+  idade: 18,
+  endereco: {
+    cidade: "Pelotas",
+    uf: "RS"
+  }
+};
+
+var mostraNome = function mostraNome(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  return console.log(nome, idade);
+};
+
+mostraNome(user);
--- JSES6/rocketseat/main.js
+++ JSES6/rocketseat/main.js
@@ -1,25 +1,12 @@
-const arr = [1,3,4,5,8,10];
+const user = {
+    nome: "Davi",
+    idade: 18,
+    endereco: {
+        cidade: "Pelotas",
+        uf: "RS",
+    },
+};
 
-const newArr = arr.map(function(item, index) {
-    return item+index;
-});
+ const mostraNome = ({ nome, idade }) => (console.log(nome, idade));
 
-console.log(newArr);
-
-const sum = arr.reduce(function(total, next) {
-return total+next;
-});
-
-console.log(sum);
-
-const filter = arr.filter(function(item) {
-return item % 2 === 0;
-});
-
-console.log(filter);
-
-const find = arr.find(function(item) {
-    return item === 2;
-});
-
-console.log(find);
+ mostraNome(user);
